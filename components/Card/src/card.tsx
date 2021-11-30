7640f63642e5a788d915ffb09d78f5296a04d3b9
ginokok1996 <ginokok1996@hotmail.com>
2021-11-30 10:38:05 +0100

    update to card

--- components/Card/src/card.tsx
+++ components/Card/src/card.tsx
@@ -19,9 +19,7 @@ export default function Card(props: CardProps) {
             <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title">{props.title}</h4>
           </div>
           <div className="col-6 text-right">
-            {props.cardHeader !== null &&
-              props.cardHeader !== undefined &&
-              props.cardHeader.map((item) => <>{item.render()}</>)}
+            {props.cardHeader !== null && props.cardHeader.map((item) => <>{item.render()}</>)}
           </div>
         </div>
       </div>
@@ -33,3 +31,7 @@ export default function Card(props: CardProps) {
     </div>
   );
 }
+
+Card.defaultProps = {
+  cardHeader: null,
+};
