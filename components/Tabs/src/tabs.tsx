7e858896270bd3a05b6a9cc1c81ff35ca185afe1
Sarai Misidjan <Sarai.misidjan@hotmail.com>
2021-11-30 12:00:46 +0100

    1.0.25

--- components/Tabs/src/tabs.tsx
+++ components/Tabs/src/tabs.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 interface TabsProps {
-  items: Array<Partial<Record<"name"|"active"|"id", any>>>;
+  items: Array<Partial<Record<"name" | "active" | "id", any>>>;
 }
 
 /**
@@ -9,20 +9,26 @@ interface TabsProps {
  * @returns TSX of the generated tabs.
  */
 export default function Tabs(props: TabsProps) {
-    const navItems = props.items.map((item) =>
-        <li className="nav-item" role="presentation" key={item.name}>
-          <a className={item.active !== undefined && item.active ? 'nav-link active' : 'nav-link'} id={item.id + "-tab"} data-bs-toggle="tab" data-bs-target={"#"+item.id}
-            role="tab" aria-controls={item.id} aria-selected="true">{item.name}
-          </a>
-        </li>
-    )
+  const navItems = props.items.map((item) => (
+    <li className="nav-item" role="presentation" key={item.name}>
+      <a
+        className={item.active !== undefined && item.active ? "nav-link active" : "nav-link"}
+        id={item.id + "-tab"}
+        data-bs-toggle="tab"
+        data-bs-target={"#" + item.id}
+        role="tab"
+        aria-controls={item.id}
+        aria-selected="true"
+      >
+        {item.name}
+      </a>
+    </li>
+  ));
 
   return (
     <div className="page-navs bg-white">
       <ul className="nav nav-tabs nav-line d-flex w-100" id="myTab" role="tablist">
-        {
-          navItems
-        }
+        {navItems}
       </ul>
     </div>
   );
