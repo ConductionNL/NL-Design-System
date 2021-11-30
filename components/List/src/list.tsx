20efb778c6bdac22a02a084744f1d5baf0788845
dev-hamid <gh.hamid89@gami.com>
2021-11-30 15:52:26 +0100

    added List Component

---
+++ components/List/src/list.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+interface ListProps {
+  items: Array<Record<"name", any>>;
+}
+
+/**
+ * This components renders bootstrap list.
+ *
+ * @returns JSX of the generated List.
+ */
+export default function List(props: ListProps) {
+
+  const liItems = props.items.map((item) =>
+    <li className="list-group-item" key={item.name} >{item.name}</li>
+  )
+  return (
+    <section className="utrecht-html">
+      <ul className="list-group-item">
+        {
+          liItems
+        }
+      </ul>
+    </section>
+  );
+}
+
+
+
