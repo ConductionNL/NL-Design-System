bd7115fa34d412c7efa6994f03f349a974c6f9b4
Sarai Misidjan <Sarai.misidjan@hotmail.com>
2021-12-01 11:17:06 +0100

    1.0.33

--- components/ActionMenu/src/actionMenu.tsx
+++ components/ActionMenu/src/actionMenu.tsx
@@ -11,29 +11,22 @@ interface ActionMenuProps {
  * @returns TSX of the generated ActionMenu.
  */
 export default function ActionMenu(props: ActionMenuProps) {
-
-  const actionItems = props.items.map((item) =>
+  const actionItems = props.items.map((item) => (
     <li className="utrecht-sidenav__item" key={item.name}>
       {item.render()}
     </li>
-  )
+  ));
 
   return (
     <nav className="utrecht-sidenav">
-      <ul className="utrecht-sidenav__list">
-        {
-          actionItems
-        }
-      </ul>
-      {
-        props.pageDescription !== null &&
+      <ul className="utrecht-sidenav__list">{actionItems}</ul>
+      {props.pageDescription !== null && (
         <>
-          <br /><br />
-          <p className="utrecht-paragraph">
-            {props.pageDescription}
-          </p>
+          <br />
+          <br />
+          <p className="utrecht-paragraph">{props.pageDescription}</p>
         </>
-      }
+      )}
     </nav>
   );
 }
