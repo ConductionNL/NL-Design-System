a46eb39b55644a16aa14cea245eb5eb2b2b9ef29
Sarai Misidjan <Sarai@conduction.nl>
2021-12-06 13:43:56 +0100

    added breakpoint to bottom navigation component

--- components/BottomNavigation/src/bottomNavigation.tsx
+++ components/BottomNavigation/src/bottomNavigation.tsx
@@ -1,32 +1,35 @@
 import * as React from "react";
 import "../../style/bottomNavigation.css";
 
-// export enum Breakpoint {
-//   mobile = "sm",
-//   tablet = "md",
-//   desktop = "lg",
-// }
+export enum Breakpoint {
+  mobile = "small",
+  tablet = "medium",
+  desktop = "large",
+}
 
 interface BottomNavigationProps {
-  items: Array<Partial<Record<"name" | "render", any>>>;
+  items: Array<Partial<Record<"name" | "icon" | "link", any>>>;
   pageDescription?: string;
-  // breakpoint?: Breakpoint;
+  breakpoint?: Breakpoint;
+  iconSize: string;
 }
 
 /**
  * This components renders a horizontal menu.
  *
- * @returns TSX of the generated BottomNavigation.
+ * @returns JSX of the generated BottomNavigation.
  */
 export default function BottomNavigation(props: BottomNavigationProps) {
   const navigationItems = props.items.map((item) => (
-    <li className="utrecht-sidenav__item" key={item.name}>
-      {item.render()}
-    </li>
+    <a href={item.link}>
+      <li className="utrecht-sidenav__item" key={item.name}>
+        <span><i className={item.icon} /> {item.name}</span>
+      </li>
+    </a>
   ));
 
   return (
-    <nav className={`utrecht-sidenav`}>
+    <nav className={`utrecht-sidenav ${props.breakpoint}`}>
       <ul className="utrecht-sidenav__list">{navigationItems}</ul>
       {props.pageDescription !== null && (
         <>
