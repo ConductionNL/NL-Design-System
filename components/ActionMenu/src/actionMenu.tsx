f53210f0f6bab94520518db8227456c372b64c2f
Sarai Misidjan <Sarai@conduction.nl>
2021-12-07 15:29:51 +0100

    fix

--- components/ActionMenu/src/actionMenu.tsx
+++ components/ActionMenu/src/actionMenu.tsx
@@ -23,7 +23,7 @@ export function ActionMenu(props: ActionMenuProps) {
   const navigationItems = props.items.map((item) => (
     <a href={item.link} key={item.name}>
       <li className="utrecht-sidenav__item" key={item.name}>
-        <span>
+        <span className="sidenav-span">
           <i className={item.icon} />
         </span>{" "}
         {item.name}
