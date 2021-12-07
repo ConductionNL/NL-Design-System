5e9b18aeded36198b50ec0092b049e139c151499
Barry Brands <barrybrands02@hotmail.com>
2021-12-07 14:05:03 +0100

    fix

--- components/Card/src/card.tsx
+++ components/Card/src/card.tsx
@@ -16,20 +16,22 @@ export function Card(props: CardProps) {
     <div className="utrecht-card card">
       <div className="utrecht-card-header card-header">
         <div className="utrecht-card-head-row card-head-row row">
-          {props.cardHeader !== null ? <>
-            <div className="col-6">
-              <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title text-start">
-                {props.title}
-              </h4>
-            </div>
-            <div className="col-6 text-right">{props.cardHeader !== null && props.cardHeader}</div></> 
-            ) : (
+          {props.cardHeader !== null ? (
+            <>
+              <div className="col-6">
+                <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title text-start">
+                  {props.title}
+                </h4>
+              </div>
+              <div className="col-6 text-right">{props.cardHeader !== null && props.cardHeader}</div>
+            </>
+          ) : (
             <div className="col-12">
               <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title text-start">
                 {props.title}
               </h4>
             </div>
-            )}
+          )}
         </div>
       </div>
       <div className="utrecht-card-body card-body">{props.cardBody !== null && props.cardBody}</div>
