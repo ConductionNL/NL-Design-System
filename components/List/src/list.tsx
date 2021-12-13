14c3cfbc45d3ad0fe88acff3f84b27d6eb6ce43e
Sarai Misidjan <Sarai@conduction.nl>
2021-12-13 11:04:32 +0100

    update list component

--- components/List/src/list.tsx
+++ components/List/src/list.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 
 interface ListProps {
   items: Array<Partial<Record<"name" | "value", any>>>;
+  link?: boolean;
+  href?: string;
+  group?: boolean;
+  groupFlush?: boolean;
+  icon?: any;
 }
 
 /**
@@ -11,13 +16,20 @@ interface ListProps {
  */
 export function List(props: ListProps) {
   const liItems = props.items.map((item) => (
-    <li className="list-item" key={item.name}>
-      {item.value !== null ? `${item.name}: ${item.value}` : item.name}
-    </li>
+    props.link ? (
+      <a href={props.href !== null ? props.href : '#'} className="list-group-item list-group-item-action" key={item.name}>
+        {item.value == null ? item.name : `${item.name}: ${item.value}`}
+        {props.icon !== null && props.icon()}
+      </a>
+    ) : (
+      <li className={props.group ? "list-group-item" : "list-item"} key={item.name}>
+        {item.value == null ? item.name : `${item.name}: ${item.value}`}
+      </li>
+    )
   ));
   return (
     <section className="utrecht-html">
-      <ul className="list">{liItems}</ul>
+      <ul className={props.group && props.groupFlush ? `list-group list-group-flush` : "list" }>{liItems}</ul>
     </section>
   );
 }
