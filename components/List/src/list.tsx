92510cf64aef60a82a17fefad09d010ef198610e
Sarai Misidjan <Sarai@conduction.nl>
2021-12-13 11:34:08 +0100

    fix

--- components/List/src/list.tsx
+++ components/List/src/list.tsx
@@ -14,22 +14,22 @@ interface ListProps {
  * @returns JSX of the generated List.
  */
 export function List(props: ListProps) {
-  const liItems = props.items.map((item) => (
+  const liItems = props.items.map((item) =>
     props.link ? (
-      <a href={item.href !== null ? item.href : '#'} className="list-group-item list-group-item-action" key={item.name}>
-        {item.value == null ? item.name : `${item.name}: ${item.value}`}
+      <a href={item.href !== null ? item.href : "#"} className="list-group-item list-group-item-action" key={item.name}>
+        {item.value === null ? item.name : `${item.name}: ${item.value}`}
         {props.icon !== null && <span className="list-icon">{props.icon()}</span>}
       </a>
     ) : (
       <li className={props.group ? "list-group-item" : "list-item"} key={item.name}>
-        {item.value == null ? item.name : `${item.name}: ${item.value}`}
+        {item.value === null ? item.name : `${item.name}: ${item.value}`}
         {props.icon !== null && <span className="list-icon">{props.icon()}</span>}
       </li>
-    )
-  ));
+    ),
+  );
   return (
     <section className="utrecht-html">
-      <ul className={props.group && props.groupFlush ? `list-group list-group-flush` : "list" }>{liItems}</ul>
+      <ul className={props.group && props.groupFlush ? `list-group list-group-flush` : "list"}>{liItems}</ul>
     </section>
   );
 }
