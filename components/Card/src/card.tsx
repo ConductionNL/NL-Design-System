75a61c783a23c2e9dc49dd220b700e4c2134eb8d
Barry Brands <barrybrands02@hotmail.com>
2021-12-13 15:47:10 +0100

    Card improvement

--- components/Card/src/card.tsx
+++ components/Card/src/card.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import "../../style/card.css";
 
 interface CardProps {
-  title: string;
+  title?: string;
   cardHeader?: any;
   cardBody: any;
+  divider?: boolean;
 }
 
 /**
@@ -12,11 +13,26 @@ interface CardProps {
  * @returns JSX of the generated Card.
  */
 export function Card(props: CardProps) {
+
+  const removeDivider = () => {
+    let style;
+    if (props.divider === false) {
+      style = {
+        borderBottom: "0 !important",
+        paddingBottom: "0 !important"
+      }
+    } else {
+      style = {};
+    }
+    return style;
+    
+  }
+
   return (
     <div className="utrecht-card card">
       <div className="utrecht-card-header card-header">
-        <div className="utrecht-card-head-row card-head-row row">
-          {props.cardHeader !== null ? (
+        <div className="utrecht-card-head-row card-head-row row" style={removeDivider} >
+          { props.title !== null && props.cardHeader !== null && (
             <>
               <div className="col-6">
                 <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title text-start">
@@ -25,7 +41,8 @@ export function Card(props: CardProps) {
               </div>
               <div className="col-6 text-right">{props.cardHeader !== null && props.cardHeader()}</div>
             </>
-          ) : (
+          )}
+          {props.title !== null && props.cardHeader === null && (
             <div className="col-12">
               <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title text-start">
                 {props.title}
