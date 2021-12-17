94984d09ac4e1a471fe048f1190a9b8f926f903a
ginokok1996 <ginokok1996@hotmail.com>
2021-12-17 10:30:52 +0100

    test

--- components/MultiDimenionalArrayInput/src/multiDimensionalArrayInput.tsx
+++ components/MultiDimenionalArrayInput/src/multiDimensionalArrayInput.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as _ from "lodash";
 
 interface MultiDimensionalArrayInputProps {
-  data: Record<any, any>;
+  data: Record<string, string>;
   id?: string;
   label?: string;
   deleteFunction?: any;
