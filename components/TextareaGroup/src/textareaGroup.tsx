6183ab2049414d3687953a44d88c1db15615bfb2
GitHub Version patch <>
2022-02-10 13:07:38 +0000

    1.0.120

--- components/TextareaGroup/src/textareaGroup.tsx
+++ components/TextareaGroup/src/textareaGroup.tsx
@@ -2,12 +2,12 @@ import * as _ from "lodash";
 import * as React from "react";
 
 interface TextareaGroupProps {
-  id: string,
-  name: string,
-  defaultValue?: string,
-  label?: string,
-  required?: boolean,
-  disabled?: boolean,
+  id: string;
+  name: string;
+  defaultValue?: string;
+  label?: string;
+  required?: boolean;
+  disabled?: boolean;
 }
 
 /**
@@ -15,15 +15,7 @@ interface TextareaGroupProps {
  *
  * @returns Jsx of the generated form.
  */
-export const TextareaGroup: React.FC<TextareaGroupProps> = (
-  {
-    id,
-    defaultValue,
-    name,
-    label,
-    required,
-    disabled,
-  }) => {
+export const TextareaGroup: React.FC<TextareaGroupProps> = ({ id, defaultValue, name, label, required, disabled }) => {
   return (
     <div className="form-group">
       <label htmlFor={id} className="utrecht-form-label">
@@ -31,7 +23,11 @@ export const TextareaGroup: React.FC<TextareaGroupProps> = (
         {required && " *"}
       </label>
 
-      <textarea maxLength={500} className="utrecht-textarea form-control" {...{ id, defaultValue, name, required, disabled }} />
+      <textarea
+        maxLength={500}
+        className="utrecht-textarea form-control"
+        {...{ id, defaultValue, name, required, disabled }}
+      />
     </div>
   );
 };
