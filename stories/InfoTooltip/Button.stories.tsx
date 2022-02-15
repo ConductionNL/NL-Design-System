81e0d9b228c921c361b197cb6000f268ee9e5139
Mark West <markwesterweel@conduction.nl>
2022-02-15 09:12:11 +0100

    Ready for review

--- stories/InfoTooltip/Button.stories.tsx
+++ stories/InfoTooltip/Button.stories.tsx
@@ -5,7 +5,7 @@ import { InfoTooltip } from "../../components/InfoTooltip/InfoTooltip";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/InfoTooltip",
+  title: "Components/InfoTooltip",
   component: InfoTooltip,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
