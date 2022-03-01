b823403bb5be9c6105c560d5ecee18b1c5303870
Hamid <gh.hamid89@gmail.com>
2022-03-01 11:04:08 +0100

    InfoTooltip added aan textareaGroup.tsx

--- components/TextareaGroup/src/textareaGroup.tsx
+++ components/TextareaGroup/src/textareaGroup.tsx
@@ -1,5 +1,11 @@
 import * as _ from "lodash";
 import * as React from "react";
+import { InfoTooltip } from "../../InfoTooltip/InfoTooltip";
+
+interface IInfoTooltip {
+  content: JSX.Element;
+  placement?: "top" | "right" | "bottom" | "left";
+}
 
 interface TextareaGroupProps {
   id: string;
@@ -8,6 +14,7 @@ interface TextareaGroupProps {
   label?: string;
   required?: boolean;
   disabled?: boolean;
+  infoTooltip?: IInfoTooltip;
 }
 
 /**
@@ -15,19 +22,35 @@ interface TextareaGroupProps {
  *
  * @returns Jsx of the generated form.
  */
-export const TextareaGroup: React.FC<TextareaGroupProps> = ({ id, defaultValue, name, label, required, disabled }) => {
-  return (
-    <div className="form-group">
-      <label htmlFor={id} className="utrecht-form-label">
-        {_.upperFirst(label ?? name)}
-        {required && " *"}
-      </label>
+export const TextareaGroup: React.FC<TextareaGroupProps> =
+  (
+    {
+      id,
+      infoTooltip,
+      defaultValue,
+      name,
+      label,
+      required,
+      disabled
+    }) => {
+    return (
+      <div className="form-group">
+        <label htmlFor={id} className="utrecht-form-label">
+          {_.upperFirst(label ?? name)}
+          {required && " *"}
+          {infoTooltip &&
+            <InfoTooltip
+              content={infoTooltip.content}
+              placement={infoTooltip.placement}
+            />
+          }
+        </label>
 
-      <textarea
-        maxLength={500}
-        className="utrecht-textarea form-control"
-        {...{ id, defaultValue, name, required, disabled }}
-      />
-    </div>
-  );
-};
+        <textarea
+          maxLength={500}
+          className="utrecht-textarea form-control"
+          {...{ id, defaultValue, name, required, disabled }}
+        />
+      </div>
+    );
+  };
